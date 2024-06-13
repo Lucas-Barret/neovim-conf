b903a85c3818ba79f65c639f5fc71712ac4b896a
Lucas-Barret <69370335+Lucas-Barret@users.noreply.github.com>
2024-06-13 08:21:29 +0400

    Add gitsigns

--- lua/plugins/git-stuffs.lua
+++ lua/plugins/git-stuffs.lua
@@ -1,8 +1,20 @@
 return {
-	"NeogitOrg/neogit",
-	dependencies = {
-		"nvim-lua/plenary.nvim", -- required
-		"sindrets/diffview.nvim", -- optional - Diff integration
-	},
-	config = true,
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+    },
+    config = function()
+      require('neogit').setup()
+      vim.keymap.set("n", "<leader>df", ":DiffviewOpen<CR>", {})
+    end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+      vim.keymap.set("n", "<leader>gp", ":GitSigns preview_hunk<CR>", {})
+    end,
+  },
 }
