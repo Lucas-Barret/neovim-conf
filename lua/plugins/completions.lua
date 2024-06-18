2287246d7114340d4c7054b4151611bca8bfb5aa
Lucas-Barret <69370335+Lucas-Barret@users.noreply.github.com>
2024-06-18 08:28:48 +0400

    add buffer as a completions engine

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,7 +1,14 @@
 return {
   {
     "L3MON4D3/LuaSnip",
-    dependencies = { "saadparwaiz1/cmp_luasnip", "rafamadriz/friendly-snippets" },
+    dependencies = { "saadparwaiz1/cmp_luasnip"}, 
+    {"rafamadriz/friendly-snippets" },   
+    { "hrsh7th/nvim-cmp" }, -- Required
+    { "hrsh7th/cmp-nvim-lsp" }, -- Required
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+    { "hrsh7th/cmp-cmdline" },
+    { "saadparwaiz1/cmp_luasnip" },
   },
   {
     "hrsh7th/nvim-cmp",
@@ -33,6 +40,8 @@ return {
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "luasnip" }, -- For luasnip users.
+          { name = "buffer" }, -- For luasnip users.
+
           -- { name = 'ultisnips' }, -- For ultisnips users.
           -- { name = 'snippy' }, -- For snippy users.
         }, {
