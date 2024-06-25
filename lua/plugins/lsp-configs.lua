4c472926c0e72b3df8091d0a8ed59a828f909a38
Lucas-Barret <69370335+Lucas-Barret@users.noreply.github.com>
2024-06-25 08:03:56 +0400

    add python support

--- lua/plugins/lsp-configs.lua
+++ lua/plugins/lsp-configs.lua
@@ -1,34 +1,51 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ruby_lsp", "tsserver", "rubocop" }, -- add ruby lsp after
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.ruby_lsp.setup({})
-      lspconfig.rubocop.setup({
-        cmd = { "bundle", "exec", "rubocop", "--lsp" },
-      })
-      lspconfig.tsserver.setup({
-        filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-        cmd = { "typescript-language-server", "--stdio" },
-      })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"ruby_lsp",
+					"tsserver",
+					"rubocop",
+					"pyright",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.ruby_lsp.setup({})
+			lspconfig.rubocop.setup({
+				cmd = { "bundle", "exec", "rubocop", "--lsp" },
+			})
+			lspconfig.tsserver.setup({
+				filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+				cmd = { "typescript-language-server", "--stdio" },
+			})
+
+      local util = require('lspconfig/util')
+      local path = util.path
+			lspconfig.pyright.setup({
+				filetypes = { "python" },
+				before_init = function(_, config)
+					local default_venv_path = path.join(vim.env.HOME, "nvim-venv", "bin", "python")
+					config.settings.python.pythonPath = default_venv_path
+				end,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,13 +6,16 @@ return {
 
 			sources = {
 				null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.rubocop,
-        null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.formatting.tsserver,
-        null_ls.builtins.diagnostics.tsserver
-			}
+				null_ls.builtins.formatting.rubocop,
+				null_ls.builtins.diagnostics.rubocop,
+				null_ls.builtins.formatting.tsserver,
+				null_ls.builtins.diagnostics.tsserver,
+				null_ls.builtins.diagnostics.black,
+				null_ls.builtins.formatting.black,
+			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    vim.keymap.set("n", "<leader>fmp", ":!black %<cr>", {})
 		vim.keymap.set("n", "<leader>gn", vim.diagnostic.goto_next, {})
 	end,
 }
