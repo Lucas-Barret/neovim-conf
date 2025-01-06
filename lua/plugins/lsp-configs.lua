5d7a25c36515286f4343bfae2c3cec32e3c240fb
Lucas-Barret <69370335+Lucas-Barret@users.noreply.github.com>
2025-01-06 09:39:32 +0400

    feat: add flash to better nav

--- lua/plugins/lsp-configs.lua
+++ lua/plugins/lsp-configs.lua
@@ -1,51 +1,55 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"ruby_lsp",
-					"tsserver",
-					"rubocop",
-					"pyright",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.ruby_lsp.setup({})
-			lspconfig.rubocop.setup({
-				cmd = { "bundle", "exec", "rubocop", "--lsp" },
-			})
-			lspconfig.tsserver.setup({
-				filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-				cmd = { "typescript-language-server", "--stdio" },
-			})
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "ruby_lsp",
+          "tsserver",
+          "rubocop",
+          "pyright",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.ruby_lsp.setup({
+        capabilites = capabilities,
+      })
+      lspconfig.rubocop.setup({
+        cmd = { "bundle", "exec", "rubocop", "--lsp" },
+      })
+      lspconfig.tsserver.setup({
+        filetypes = { "typescript", "typescriptreact", "javascriptreact","typescript.tsx" },
+        cmd = { "typescript-language-server", "--stdio" },
+      })
 
-      local util = require('lspconfig/util')
+      local util = require("lspconfig/util")
       local path = util.path
-			lspconfig.pyright.setup({
-				filetypes = { "python" },
-				before_init = function(_, config)
-					local default_venv_path = path.join(vim.env.HOME, "nvim-venv", "bin", "python")
-					config.settings.python.pythonPath = default_venv_path
-				end,
-			})
+      lspconfig.pyright.setup({
+        filetypes = { "python" },
+        before_init = function(_, config)
+          local default_venv_path = path.join(vim.env.HOME, "nvim-venv", "bin", "python")
+          config.settings.python.pythonPath = default_venv_path
+        end,
+      })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,27 +1,41 @@
 return {
-  {
+	{
 
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.6",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
+		config = function()
+			local telescope = require("telescope")
+
       local builtin = require("telescope.builtin")
+
+			telescope.setup({
+				defaults = {
+					path_display = {"smart"},
+				},
+			})
+
+      telescope.load_extension("fzf")
+
       vim.keymap.set("n", "<leader><leader>", builtin.live_grep, {})
       vim.keymap.set("n", "<D-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-    end,
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
